b0f3554823ff58bd5522a74e153cd16623aeb7e7
tomoki_sun <tomoki69386@gmail.com>
2019-12-20 08:41:20 +0900

    Add TableComponent Tests

---
+++ SwiftyKitTests/TableComponentTests.swift
@@ -0,0 +1,25 @@
+//
+//  TableComponentTests.swift
+//  SwiftyKitTests
+//
+//  Created by 築山朋紀 on 2019/12/20.
+//  Copyright © 2019 tomoki_sun. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyKit
+
+class TableComponentTests: XCTestCase {
+    
+    func testEnumTableSection() {
+        enum Color: Int, CaseIterable, TableSection {
+            case red, blue, green, yellow
+        }
+        
+        XCTAssertEqual(Color.count, 4)
+        XCTAssertEqual(Color(0), Color.red)
+        XCTAssertEqual(Color(1), Color.blue)
+        XCTAssertEqual(Color(2), Color.green)
+        XCTAssertEqual(Color(3), Color.yellow)
+    }
+}
