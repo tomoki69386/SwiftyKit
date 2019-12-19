67f2f122e67df825d8e38a1913b520191a654275
tomoki_sun <tomoki69386@gmail.com>
2019-12-19 13:48:15 +0900

    Initial Commit

---
+++ SwiftyKit/SwiftyKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyKit.h
+//  SwiftyKit
+//
+//  Created by 築山朋紀 on 2019/12/19.
+//  Copyright © 2019 tomoki_sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyKit.
+FOUNDATION_EXPORT double SwiftyKitVersionNumber;
+
+//! Project version string for SwiftyKit.
+FOUNDATION_EXPORT const unsigned char SwiftyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyKit/PublicHeader.h>
+
+
