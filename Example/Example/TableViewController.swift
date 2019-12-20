3a80bd3f76a4d81daf361f7ebba5ea2cb9241b0f
tomoki_sun <tomoki69386@gmail.com>
2019-12-20 09:43:23 +0900

    Update Example

--- Example/Example/TableViewController.swift
+++ Example/Example/TableViewController.swift
@@ -15,15 +15,29 @@ final class TableViewController: UITableViewController {
         super.viewDidLoad()
         
         self.tableView.register(cellClass: TableViewCell.self)
+        self.tableView.register(nibClass: NibTableViewCell.self)
+    }
+    
+    enum Section: Int, CaseIterable, TableSection {
+        case nib, code
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(of: TableViewCell.self, for: indexPath)
-        cell.textLabel?.text = "\(indexPath.section): \(indexPath.row)"
-        return cell
+        switch Section(indexPath.section) {
+        case .nib:
+            return tableView.dequeueReusableCell(of: NibTableViewCell.self, for: indexPath)
+        case .code:
+            let cell = tableView.dequeueReusableCell(of: TableViewCell.self, for: indexPath)
+            cell.textLabel?.text = "\(indexPath.section): \(indexPath.row)"
+            return cell
+        }
     }
 }
